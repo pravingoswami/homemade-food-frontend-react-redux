b3eb30e1f6c9e4f5dc5339beffe97f2ae4c0abe9
pravingoswami <goswami4737@gmail.com>
2020-06-01 10:05:54 +0530

    simple setup of react with the redux store

---
+++ src/redux/reducers/userReducers.js
@@ -0,0 +1,11 @@
+const userInitialState = []
+
+const userReducers = (state = userInitialState, action ) => {
+    switch(action.type){
+        default : {
+            return state
+        }
+    }
+}
+
+export default userReducers
