cda50385252e0629065a704b235f77958528aec3
pravingoswami <goswami4737@gmail.com>
2020-06-02 10:21:44 +0530

    user login form created and storing into the formdata

--- src/components/login/LoginUser.js
+++ src/components/login/LoginUser.js
@@ -1,11 +1,48 @@
 import React from "react"
 
-function LoginUser(props) {
-    return (
-        <div>
-            <h1>This is Login Page</h1>
-        </div>
-    )
+class LoginUser extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            email : "",
+            password : ""
+        }
+    }
+
+    handleInputChange = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = {
+            email : this.state.email,
+            password : this.state.password
+        }
+        console.log(formData)
+    }
+
+    render(){
+
+        return (
+            <div>
+                <h1>This is Login Page</h1>
+                <form onSubmit = {this.handleSubmit} >
+                    <label>Email</label>&nbsp;&nbsp;
+                    <input type = "text" name = "email" value = {this.state.email} onChange = {this.handleInputChange} />
+                    <br></br>
+                    <br></br>
+                    <label>Pasword</label>&nbsp;&nbsp;
+                    <input type = "password" name = "password" value = {this.state.password} onChange = {this.handleInputChange} />
+                    <br></br>
+                    <br></br>
+                    <button type = "submit" >SUBMIT</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default LoginUser
