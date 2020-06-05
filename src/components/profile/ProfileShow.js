4eb07dbf2869390f0402f88ed8c2bb568581d16f
pravingoswami <goswami4737@gmail.com>
2020-06-05 19:34:14 +0530

    getting user profile information from the redux store

--- src/components/profile/ProfileShow.js
+++ src/components/profile/ProfileShow.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 function ProfileShow(props){
     return (
-        <div>
+        <div> {console.log(props.user)}
             <h1>This is User Profile Show Page</h1>
+            {
+                props.user && (
+                    <div>
+                        <label><b>Name</b> - {props.user.name}</label>
+                        <br></br> 
+                        <label><b>Username</b> - {props.user.username}</label>
+                        <br></br>
+                        <label><b>Mobile</b> - {props.user.mobile}</label>
+                        <br></br>
+                        <label><b>Email</b> - {props.user.email}</label>
+                        <br></br>
+                        <label><b>Gender</b> - {props.user.gender}</label>
+                        <br></br>
+                    </div>
+                )
+            }
         </div>
     )
 }
 
-export default ProfileShow
+const mapStateToProps = (state) => {
+    return {
+        user : state.user
+    }
+}
+
+export default connect(mapStateToProps)(ProfileShow)
